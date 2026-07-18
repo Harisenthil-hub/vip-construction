2399034cd96d46d7e11c599f868ddd89c3debf66
Harisenthil-hub <hari367854@gmail.com>
2026-07-18 19:38:04 +0530

    feat: add desktop top bar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
-
+import { TopBar } from "../components/TopBar";
 
 export function MainLayout() {
   return (
     <div>
-      
+      {/* TOP HEADER */}
+      <TopBar />
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home/Home";
 import { Service } from "../pages/service/Service";
-
+import { MainLayout } from "../layouts/MainLayout";
 
 export function AppRoutes() {
   return (
     <Routes>
+      <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/service" element={<Service />} />
+      </Route>
     </Routes>
   );
 }
