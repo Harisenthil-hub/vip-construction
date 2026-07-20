c05138e065ca4ea6c6b84d466dd25d41885590d3
DHARANESH001 <dharaneshv001@gmail.com>
2026-07-20 14:22:07 +0530

    first commit

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home/Home";
 import { Service } from "../pages/service/Service";
-
+import { Project } from "../pages/project/Project";
 
 export function AppRoutes() {
   return (
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/service" element={<Service />} />
+        <Route path="/project" element={<Project/>} />
     </Routes>
   );
 }
