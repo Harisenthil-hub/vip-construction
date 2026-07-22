2721e4abde8daf2d54fa11ec93fd805a13a5eb84
Harisenthil-hub <hari367854@gmail.com>
2026-07-22 21:03:45 +0530

    feat: Homepage Service section done with responsiveness

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import { HeroSection } from "./components/HeroSection";
+import { ServiceSection } from "./components/ServiceSection";
 
 export function Home() {
   return (
     <div className="">
 
       <HeroSection />
+
+      <ServiceSection />
     </div>
   );
 }
