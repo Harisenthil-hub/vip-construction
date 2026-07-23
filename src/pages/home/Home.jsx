06615dae6ca36a0475d6017a07dbaf54210c0d8b
Harisenthil-hub <hari367854@gmail.com>
2026-07-23 16:40:54 +0530

    feat: Certificate Section with responsiveness

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import { HeroSection } from "./components/HeroSection";
 import { ServiceSection } from "./components/ServiceSection";
 import Projects from '../service/components/Projects'
+import { Certifications } from "./components/Certifications";
 export function Home() {
   return (
     <div className="">
@@ -10,6 +11,8 @@ export function Home() {
       <ServiceSection />
 
       <Projects />
+
+      <Certifications />
     </div>
   );
 }
